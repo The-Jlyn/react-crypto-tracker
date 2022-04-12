5d92760759ff2869756aa735b747687f91ddf897
The-Jlyn <99376158+The-Jlyn@users.noreply.github.com>
2022-04-12 14:46:01 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -62,6 +62,7 @@ function Header() {
             >
               <MenuItem value={"USD"}>USD</MenuItem>
               <MenuItem value={"INR"}>INR</MenuItem>
+              <MenuItem value={"EUR"}>EUR</MenuItem>
             </Select>
           </Toolbar>
         </Container>
